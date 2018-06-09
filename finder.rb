f09a1f571cc561f9afa12f574f16f58cad888e95
Anda Popovici <andap2002@gmail.com>
2018-06-09 18:37:48 +0100

    Some refactoring

--- finder.rb
+++ finder.rb
@@ -2,16 +2,6 @@ require "httparty"
 require "nokogiri"
 
 require "./mailer"
+require "./zoopla"
 
-p "Getting Zoopla..."
-doc = HTTParty.get("https://www.zoopla.co.uk/to-rent/property/edinburgh/?beds_min=2&include_shared_accommodation=false&price_frequency=per_month&price_max=1000&q=Edinburgh&results_sort=newest_listings&search_source=home")
-
-@parsed = Nokogiri::HTML(doc)
-ugly_data = @parsed.xpath('//ul[contains(@class, "listing-results")]/li/@data-listing-id')
-results = ugly_data.map { |id| "https://www.zoopla.co.uk/to-rent/details/#{id.value}" }
-ids = ugly_data.map { |id| id.value }
-
-p "Found #{results.length} results"
-p results
-
-Mailer.new.send(results)
+get_zoopla(notify: false)
---
+++ zoopla.rb
@@ -0,0 +1,16 @@
+def get_zoopla(notify: true)
+  p "Getting Zoopla..."
+  doc = HTTParty.get("https://www.zoopla.co.uk/to-rent/property/edinburgh/?beds_min=2&include_shared_accommodation=false&price_frequency=per_month&price_max=1000&q=Edinburgh&results_sort=newest_listings&search_source=home")
+
+  @parsed = Nokogiri::HTML(doc)
+  ugly_data = @parsed.xpath('//ul[contains(@class, "listing-results")]/li/@data-listing-id')
+  results = ugly_data.map { |id| "https://www.zoopla.co.uk/to-rent/details/#{id.value}" }
+  ids = ugly_data.map { |id| id.value }
+
+  p "Found #{results.length} results"
+  p results
+
+  if notify
+    Mailer.new.send(results)
+  end  
+end
