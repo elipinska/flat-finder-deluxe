d826e49d777c0051dfaa26b12921721c82e400e5
Anda Popovici <andap2002@gmail.com>
2018-06-09 13:27:54 +0100

    Add Gemfile and Rake task

--- finder.rb
+++ finder.rb
@@ -1,6 +1,7 @@
 require "HTTParty"
 require "nokogiri"
 
+p "Getting Zoopla..."
 doc = HTTParty.get("https://www.zoopla.co.uk/to-rent/property/edinburgh/?beds_min=2&include_shared_accommodation=false&price_frequency=per_month&price_max=1000&q=Edinburgh&results_sort=newest_listings&search_source=home")
 
 @parsed = Nokogiri::HTML(doc)
@@ -8,4 +9,5 @@ ugly_data = @parsed.xpath('//ul[contains(@class, "listing-results")]/li/@data-li
 results = ugly_data.map { |id| "https://www.zoopla.co.uk/to-rent/details/#{id.value}" }
 ids = ugly_data.map { |id| id.value }
 
+p "Found #{results.length} results"
 p results
